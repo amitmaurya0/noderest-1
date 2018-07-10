5198f0b6f596e85d5b9b3de1b2bdd70ed6f8ede6
Amit Maurya <king.aimmy@gmail.com>
2018-07-10 21:28:46 +0530

    a

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 	secret:"nodeauthsecret",
-	/* mongoUrl:'mongodb://kingaimmy:King_9616@ds111476.mlab.com:11476/node-rest' */
-	mongoUrl:'mongodb://127.0.0.1:27017/node-rest'
+	mongoUrl:'mongodb://kingaimmy:King_9616@ds111476.mlab.com:11476/node-rest'
+	/* mongoUrl:'mongodb://127.0.0.1:27017/node-rest' */
 }
